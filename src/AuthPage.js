62705c5e73f6aa51fbd595d2df2062c1adc2f4d5
SusanBird <susanbird391@gmail.com>
2022-05-11 17:57:49 -0700

    track state Auth page plus handlesingin()

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,15 +1,36 @@
-import { useState } from 'react';
-import { signIn, signUp } from './services/fetch-utils.js';
+import React, { useState } from 'react';
+import { signIn, signUp, getUser } from './services/fetch-utils.js';
 
-export default function AuthPage(props) {
+export default function AuthPage({ setEmail, setToken }) {
   // you'll need to track the form state of the email and password for sign in, and separate state for sign up
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+
+  const [{ 
+    email: signInEmail, 
+    password: signInPassword,
+  }, setSignInFormData] = useState({
+    email: '',
+    password: ''
+  });
 
   async function handleSignIn(e) {
     e.preventDefault();
       
     // sign the user in using the form state
+    await signIn(signInEmail, signInPassword);
 
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
+    const { 
+      access_token, 
+      user: { 
+        email,
+      } 
+    } = getUser();
+
+    setEmail(email);
+    setToken(access_token);
+  
   }
     
   async function handleSignUp(e) {
